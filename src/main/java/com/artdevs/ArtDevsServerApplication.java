015dc6ac14cefafa879149f1767412b5d4a671e3
LinhPT2108 <tuanlinho9o22o15@gmail.com>
2024-01-01 21:58:20 +0700

    base source

---
+++ src/main/java/com/artdevs/ArtDevsServerApplication.java
@@ -0,0 +1,13 @@
+package com.artdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtDevsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtDevsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artdevs/ArtDevsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtDevsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
